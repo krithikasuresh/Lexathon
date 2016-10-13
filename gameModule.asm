eff2382012b6f565a0662db203e3b4da3e100475
Oxiras <sd.moustapha@gmail.com>
2016-10-13 14:32:57 -0500

    Corrected timer code to properly end game when timer runs out.

--- gameModule.asm
+++ gameModule.asm
@@ -1,19 +1,20 @@
 # Program: gameModule.asm
 
 beginCountdown:
-	li	$s1, 60							# Initialize s1 to hold 60s
-	li	$v0, 30							# Get time of countdown start
+	li	$s1, 5						# Initialize s1 to hold 60s
+	li	$v0, 30						# Get time of countdown start
 	syscall
 	sw	$a0, startTime						# Store start time
 
 checkTime:
 	lw	$t0, startTime						# Load start time
-	li	$v0, 30							# Get current time
+	li	$v0, 30						# Get current time
 	syscall
 	sub	$s0, $a0, $t0						# Calculate difference btw current and start time
 	div	$s0, $s0, 1000						# Convert time from ms to s
 	sub	$s0, $s1, $s0						# Calculate remaining time
-	beq	$s0, $0, timeOut
+	slti	$t1, $s0, 1						# Check if remaining time is less or equal to 0
+	bne	$t1, $0, timeOut						# End game when time is up
 	li	$v0, 4
 	la	$a0, displayTime
 	syscall
